a08517e003d5eaaf7eeafa0f8dbf5e511bd65f80
Jingbei Li <i@jingbei.li>
2017-11-08 15:38:22 +0800

    fork the launcher to background

--- Plants vs. Zombies Game of the Year.app/Contents/MacOS/run.sh
+++ Plants vs. Zombies Game of the Year.app/Contents/MacOS/run.sh
@@ -5,8 +5,8 @@ rm -f $log
 touch $log
 path=$(dirname "$0")
 
+(
 find $HOME/Library/Application\ Support/Steam/userdata -path '*/3590/remote/user*.dat' -not -name 'users.dat' -exec python "$path/convert.py" mac {} 1>>$log 2>>$log \;
-
 open -W steam://run/$steamid
-
 find $HOME/Library/Application\ Support/Steam/userdata -path '*/3590/remote/user*.dat' -not -name 'users.dat' -exec python "$path/convert.py" windows {} 1>>$log 2>>$log \;
+) &
